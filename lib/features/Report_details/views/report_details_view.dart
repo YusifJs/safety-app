46f4c3675d6c12d295e0fa50d64c8c25e32aca37
mohamed samir <mohamedsamir7m@gmail.com>
2026-04-27 05:32:19 +0300

    add report details

---
+++ lib/features/Report_details/views/report_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:safety_app/features/Report_details/views/widgets/report_details_view_body.dart';
+
+class ReportDetailsView extends StatelessWidget {
+  const ReportDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ReportDetailsViewBody());
+  }
+}
