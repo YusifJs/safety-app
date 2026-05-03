7baec9b66b2c2077d5ce96de6a9a2e2900ed6472
Yousef <yusifkhaled16@gmail.com>
2026-05-03 20:47:58 +0300

    Make Dialog Image Nullable

--- lib/features/widgets/custom_dialog.dart
+++ lib/features/widgets/custom_dialog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:safety_app/core/constants/app_assets.dart';
 import 'package:safety_app/core/constants/app_colors.dart';
 import 'package:safety_app/core/constants/app_text_style.dart';
-import 'package:safety_app/features/widgets/custom_dialog_image.dart';
 import 'package:safety_app/features/widgets/custom_elevated_button.dart';
 
 class CustomDialog extends StatelessWidget {
@@ -11,21 +9,30 @@ class CustomDialog extends StatelessWidget {
   final String description;
   final String button1;
   final String button2;
-  final Widget dialogImage;
+  final Widget? dialogImage;
+
   const CustomDialog({
     super.key,
     required this.title,
     required this.description,
     required this.button1,
     required this.button2,
-    required this.dialogImage,
+    this.dialogImage,
   });
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
       child: Container(
-        padding: EdgeInsets.only(left: 12, right: 12, bottom: 52, top: 30),
+        padding: const EdgeInsets.only(
+          left: 12,
+          right: 12,
+          bottom: 52,
+          top: 30,
+        ),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: AppColors.white,
@@ -34,22 +41,33 @@ class CustomDialog extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            dialogImage,
-            SizedBox(height: 24.sp),
+            
+            // ✅ SAFE IMAGE HANDLING
+            if (dialogImage != null) dialogImage!,
+
+            if (dialogImage != null)
+              SizedBox(height: 24.sp),
+
             Text(
               title,
               style: AppTextStyle.blackBold20,
               textAlign: TextAlign.center,
             ),
+
             SizedBox(height: 16.sp),
+
             Text(
               description,
               style: AppTextStyle.blackRegular12,
               textAlign: TextAlign.center,
             ),
+
             SizedBox(height: 24.sp),
+
             CustomElevatedButton(text: button1),
+
             SizedBox(height: 8.sp),
+
             CustomElevatedButton(
               text: button2,
               backgroundColor: AppColors.transperant,
