439ee411728e1b7b3d5edda1e2bdbce3715649e6
Yousef <yusifkhaled16@gmail.com>
2026-05-03 03:26:07 +0300

    add support chat route

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:safety_app/core/routing/routes.dart';
 import 'package:safety_app/features/login/login_screen.dart';
 import 'package:safety_app/features/onboarding/onboarding_screen.dart';
+import 'package:safety_app/features/report/presentation/pages/report_pages_controller.dart';
 import 'package:safety_app/features/splash/animated_splash_screen.dart';
+import 'package:safety_app/features/support_chat/presentation/screens/support_chat_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -14,6 +16,10 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => OnboardingScreen());
       case Routes.loginScreen:
         return MaterialPageRoute(builder: (_) => LoginScreen());
+      case Routes.supportChat:
+        return MaterialPageRoute(builder: (_) => SupportChatScreen());
+      case Routes.reportScreenController:
+        return MaterialPageRoute(builder: (_) => ReportPagesController());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const String AnimatedSplashScreen = '/SplashScreen';
   static const String onBoardingScreen = '/onboarding';
   static const String loginScreen = '/login';
+  static const String supportChat = '/supportChat';
+  static const String reportScreenController = '/reportScreenController';
 }
